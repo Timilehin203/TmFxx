1d4480a718b0e255f2bf65b077549142f3c63bea
Timilehin203 <mb8117240@gmail.com>
2026-08-19 20:49:33 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,8 @@ const FRONTEND_URL =
     "https://matildamillie382-crypto.github.io";
 
 const ADMIN_UPDATE_KEY =
-    process.env.ADMIN_UPDATE_KEY;
+    process.env.ADMIN_UPDATE_KEY";
+
 
 if (!ADMIN_UPDATE_KEY) {
 
